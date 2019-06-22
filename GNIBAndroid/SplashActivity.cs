8df377c88f5e6b3b068cd090926b6ae7404f4647
Maryabeeb <10519008@mydbs.ie>
2019-06-22 17:21:19 +0100

    Recommiting the andriod platform local git,cos the first one nt showing

---
+++ GNIBAndroid/SplashActivity.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace GNIBAndroid
+{
+    //[Activity(Label = "SplashActivity")]
+    [Activity(Theme = "@style/MyTheme.Splash", MainLauncher = true, NoHistory = true)]
+
+    public class SplashActivity : Activity
+    {
+        protected override void OnResume()
+        {
+            base.OnResume();
+            StartActivity(typeof(MainActivity));
+        }
+    }
+}
