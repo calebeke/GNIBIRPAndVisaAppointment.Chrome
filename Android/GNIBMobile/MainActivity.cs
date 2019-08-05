f0e440f1680781d4fea3ceef550379177268d7ce
Maryabeeb <10519008@mydbs.ie>
2019-08-06 00:02:03 +0100

    Implementation of  some navigation items when selected using fragments

--- Android/GNIBMobile/MainActivity.cs
+++ Android/GNIBMobile/MainActivity.cs
@@ -10,39 +10,135 @@ using Android.Support.V4.Widget;
 using Android.Views;
 using Android.Support.Design.Widget;
 using Android.Support.V7.App;
+using System;
+using Xamarin.Forms.Platform.Android;
+using ActionBar = Android.App.ActionBar;
 
 namespace GNIBMobile
 {
-    [Activity(Label = "GNIB/IRP", MainLauncher = false, Icon = "@drawable/irishLogo")]
+    [Activity(Label = "GNIB/IRP", MainLauncher = false, Icon = "@drawable/irishLogo", Theme ="@style/MyTheme")]
 
-    public class MainActivity : AppCompatActivity
+    public class MainActivity : Android.Support.V7.App.AppCompatActivity
     {
         DrawerLayout drawerLayout;
-        NavigationView navigationView;
+      private  NavigationView navigationView;
+       
+        Android.Support.V4.App.Fragment nativeFragment;
 
-        protected override void OnCreate(Bundle bundle)
+        public static MainActivity Instance;
+        protected override void OnCreate(Bundle savedInstanceState)
 {
-            base.OnCreate(bundle);
+            base.OnCreate(savedInstanceState);
+
+            RequestWindowFeature(WindowFeatures.ActionBar);
+
+            ActionBar.NavigationMode = ActionBarNavigationMode.Tabs;
+
             SetContentView(Resource.Layout.Main);
-            var toolbar = FindViewById<V7Toolbar>(Resource.Id.toolbar);
+
+            var toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
+
+
+
+            //SupportActionBar.SetDisplayShowTitleEnabled(false);
+            //SupportActionBar.SetHomeButtonEnabled(true);
+            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.menuIcon2);
             SupportActionBar.SetDisplayHomeAsUpEnabled(true);
-            SupportActionBar.SetDisplayShowTitleEnabled(false);
-            SupportActionBar.SetHomeButtonEnabled(true);
-            SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.menuIcon);
+
             drawerLayout = FindViewById<DrawerLayout>(Resource.Id.drawer_layout);
-            navigationView = FindViewById<NavigationView>(Resource.Id.nav_view);
+            navigationView = FindViewById<NavigationView>(Resource.Id.navigationView1);
+            SetupDrawerContent(navigationView);
+
+
+
+
+//        }if (_trackMapFragment == null)
+//{
+//    _trackMapFragment = new TrackMapFragment();
+//        var fragmentTx = SupportFragmentManager.BeginTransaction();
+//        fragmentTx.Add(Resource.Id.frame, _trackMapFragment);
+//    fragmentTx.Commit();
+}
+       
+        void SetupDrawerContent(NavigationView navigationView)
+        {
+            navigationView.NavigationItemSelected += (sender, e) =>
+            {
+               
+
+                //Add this
+                switch (e.MenuItem.ItemId)
+                {
+                    case Resource.Id.nav_appoint:
+                        AddTab("New", new NewDateFrag());
+                        AddTab("Renewal", new RenewDateFrag());
+
+                        Toast.MakeText(this, "Action selected: Appoint" ,
+    ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.nav_cancel:
+                        Toast.MakeText(this, "Action selected: " ,
+    ToastLength.Short).Show();
+                        break;
+                    case Resource.Id.nav_preset:
+                        Forms.Init(this, null);
+                        nativeFragment = new DashFragment();
+                        break;
+                    case Resource.Id.nav_notify:
+                        Toast.MakeText(this, "Action selected: ",
+    ToastLength.Short).Show();
+                        break;
+                }
+                SupportFragmentManager
+      .BeginTransaction()
+      .AddToBackStack(null)
+      .Replace(Resource.Id.container, nativeFragment, "preset")
+      .Commit();
+
+                e.MenuItem.SetChecked(true);
+                drawerLayout.CloseDrawers();
+            };
+        }
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu, menu);
+            return base.OnCreateOptionsMenu(menu);
         }
         public override bool OnOptionsItemSelected(IMenuItem item)
         {
-            switch (item.ItemId)
+            switch(item.ItemId)
+           
             {
                 case Android.Resource.Id.Home:
                     drawerLayout.OpenDrawer(Android.Support.V4.View.GravityCompat.Start);
-                    return true;
+                    break;
+
+                //default:
+                //   breaK;
+                   
             }
-            return base.OnOptionsItemSelected(item);
+                     //drawerLayout.CloseDrawers();      
+                    return base.OnOptionsItemSelected(item);
+
+            
         }
+
+       
+        void AddTab(string tabText, Android.Support.V4.App.Fragment fragment1)
+        {
+            var tab = this.ActionBar.NewTab();
+            tab.SetText(tabText);
+           
+            // must set event handler for replacing tabs tab
+            //tab.TabSelected += delegate (object sender, Android.App.ActionBar.TabEventArgs e) {
+            //    e.FragmentTransaction.Replace(Resource.Id.container, fragment1);
+            //};
+
+            this.ActionBar.AddTab(tab);
+        }
+
+
 
     }
 }
